6576e6cc3fd90626da4a6687ce3f2b6853fbbaed
Idakwoji Theophilus <pietrosparks@gmail.com>
2018-05-22 01:51:09 +0100

    change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const serveStatic = require('serve-static');
 const server = require('http').Server(app);
 const history = require('connect-history-api-fallback');
 const port = process.env.PORT || 5000;
-app.use(serveStatic(__dirname + "../dist"));
+app.use(serveStatic(__dirname + "./dist"));
 app.use(history({verbose:true}));
 app.listen(port);
 console.log('server started '+ port);
